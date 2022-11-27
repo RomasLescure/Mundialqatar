d0f524e4624b881694246316b27a8f18a9742aa5
ralv400 <78281458+RomasLescure@users.noreply.github.com>
2022-11-27 05:04:43 -0500

    Se actualizo el registro y el login

--- login.php
+++ login.php
@@ -19,7 +19,7 @@
             echo "nombre o contraseña invalidos";
         }
     }
-    ?>
+?>
 
 
 <!DOCTYPE html>
@@ -32,24 +32,25 @@
     <link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
-    <div class="card text-white bg-primary mb-3" style="">
+    <div class="card text-white bg-primary mb-3" style="margin: 20px 20px 20px 20px">
         <div class="card-header">Inicio de sesión</div>
         <div class="card-body">
         <form action="login.php" method="post">
             <fieldset>
                 <div class="form-group">
                     <label for="usuario" class="form-label mt-4">Usuario</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" name="usuario" placeholder="Insertar usuario">
+                    <input type="text" class="form-control" name="usuario" placeholder="Insertar usuario">
                 </div>
-                <label for="apellido">Contraseña</label>
-                <input type="text" name="contraseña" placeholder="Contraseña" require>
-                <br><br>
+                <div class="form-group">
+                    <label for="contraseña" class="form-label mt-4">Contraseña</label>
+                    <input type="password" class="form-control" name="contraseña" placeholder="Insertar Contraseña">
+                </div>
+                <br>
                 <input type="submit" value="conectar">
+                <a href="registrar.php"><button>registrarse</button></a>
             </fieldset>
         </form>
         </div>
     </div>
-    <br><br>
-    <a href="registrar.php"><button>registrar</button></a>
 </body>
 </html>
--- registrar.php
+++ registrar.php
@@ -8,12 +8,37 @@
     <link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
-    <form action='registro.php' method='POST' name='formulario'>
-        Nombre: <input type='text' name='nombre'><br><br>
-        Contraseña: <input type='text' name='apellido'><br><br>
-        Tipo de usuario: 
-        <input type='submit' name='botonGuardar' value='guardar'>
-    </form>
+<div class="card text-white bg-dark mb-3" style="margin: 20px 20px 20px 20px;">
+        <div class="card-header">Inicio de sesión</div>
+        <div class="card-body">
+        <form action="login.php" method="post">
+            <fieldset>
+                <div class="form-group">
+                    <label for="usuario" class="form-label mt-4">Usuario</label>
+                    <input type="text" class="form-control" name="usuario" placeholder="Insertar usuario">
+                </div>
+                <div class="form-group">
+                    <label for="contraseña" class="form-label mt-4">Contraseña</label>
+                    <input type="password" class="form-control" name="contraseña" placeholder="Insertar Contraseña">
+                </div>
+                 <!-- Dependiendo del tipo de usuario se lleva a una pagina u a otra -->
+                <div class="form-group">
+                    <label for="exampleSelect1" class="form-label mt-4">Tipo de usuario</label>
+                    <select class="form-select" name="Tipo_Usuario">
+                        <!-- Si es predeterminado se registra automaticamente -->
+                        <option>Predeterminado</option>
+                        <!-- Si es Administrativo se debe llevar a una pagina en la cual debe colocar
+                        una contraseña que solo los administrativos saben osea nosotros, elijan ustedes la 
+                        contraseña -->
+                        <option>Administrativo</option>
+                    </select>
+                </div>
+                <br>
+                <input type="submit" value="Registrarse">
+            </fieldset>
+        </form>
+        </div>
+    </div>
 </body>
 
 </html>
