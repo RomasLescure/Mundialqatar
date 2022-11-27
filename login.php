506691c91e8648f6a43e3c3329ced66fa14845cd
rocica <rocio.cortez@utp.ac.pa>
2022-11-27 17:50:45 -0500

    cambios no funcionales

--- login.php
+++ login.php
@@ -1,29 +1,30 @@
 <?php
-    session_start();
-    $db_host = 'localhost';
-    $db_user = 'root';
-    $db_password ='';
-    $db_db = 'qatar_2022';
+session_start();
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_db = 'qatar_2022';
 
-    $con_db = mysqli_connect($db_host, $db_user, $db_password, $db_db);
-    
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $usuario = $_POST["usuario"];
-        $contraseña = $_POST["contraseña"];
-        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contraseña = '$contraseña'";
-        $result = $con_db -> query($sql);
-        if($result -> num_rows > 0){
-            $_SESSION["usuario"] = $usuario;
-            header("location: inicio.php");
-        }else{
-            echo "nombre o contraseña invalidos";
-        }
+$con_db = mysqli_connect($db_host, $db_user, $db_password, $db_db);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST["user"];
+    $pass = $_POST["pass"];
+    $sql = "SELECT * FROM usuarios WHERE user = '$user' AND pass = '$pass'";
+    $result = $con_db->query($sql);
+    if ($result->num_rows > 0) {
+        $_SESSION["user"] = $user;
+        header("location: inicio.php");
+    } else {
+        echo "Usuario o contraseña invalidos";
     }
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,26 +32,28 @@
     <title>Login</title>
     <link rel="stylesheet" href="bootstrap.min.css">
 </head>
+
 <body>
     <div class="card text-white bg-primary mb-3" style="margin: 20px 20px 20px 20px">
         <div class="card-header">Inicio de sesión</div>
         <div class="card-body">
-        <form action="login.php" method="post">
-            <fieldset>
-                <div class="form-group">
-                    <label for="usuario" class="form-label mt-4">Usuario</label>
-                    <input type="text" class="form-control" name="usuario" placeholder="Insertar usuario">
-                </div>
-                <div class="form-group">
-                    <label for="contraseña" class="form-label mt-4">Contraseña</label>
-                    <input type="password" class="form-control" name="contraseña" placeholder="Insertar Contraseña">
-                </div>
-                <br>
-                <input type="submit" value="conectar">
-                <a href="registrar.php"><button>registrarse</button></a>
-            </fieldset>
-        </form>
+            <form action="login.php" method="post">
+                <fieldset>
+                    <div class="form-group">
+                        <label for="user" class="form-label mt-4">Usuario</label>
+                        <input type="text" class="form-control" name="user" placeholder="Insertar Usuario">
+                    </div>
+                    <div class="form-group">
+                        <label for="pass" class="form-label mt-4">Contraseña</label>
+                        <input type="password" class="form-control" name="pass" placeholder="Insertar Contraseña">
+                    </div>
+                    <br>
+                    <input type="submit" value="conectar">
+                    <a href="registrar.php"><button>registrarse</button></a>
+                </fieldset>
+            </form>
         </div>
     </div>
 </body>
+
 </html>
--- registro.php
+++ registro.php
@@ -1,31 +1,28 @@
 <?php
-if(isset($_POST['nombre']) && !empty($_POST['nombre']) && isset($_POST['apellido']) &&
-!empty($_POST['apellido'])){
-    $nombreReg= $_POST['nombre'];
-    $apellidoReg=$_POST['apellido'];
+if (
+    isset($_POST['user']) && !empty($_POST['user']) && isset($_POST['pass']) &&
+    !empty($_POST['pass'])
+) {
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
 
-    $db='qatar_2022';
-    $host='localhost';
-    $pw='';
-    $user='root';
+    $db = 'qatar_2022';
+    $host = 'localhost';
+    $pw = '';
+    $user = 'root';
 
-    $con=mysqli_connect($host,$user,$pw,$db) or die ('no se pudo autentificar con la Bd');
-    mysqli_select_db($con,$db) or die ('no se puedo conectar a la BD');
-    if($capitalReg >= 100){
-        $sql="INSERT INTO cliente(nombre,apellido,capital)
-        VALUES ('$nombreReg','$apellidoReg','$capitalReg')";
-        if($con->query($sql) === TRUE){
-            echo 'guardado corretamente <br>';
-            echo '<a href=login.php> volver al inicio </a>';
-        }else{
-            echo 'error:'.$sql.'<br>'.$con->error;
-        }
-        $con->close();
-    }else{
-        echo '<br> El minimo a ingresar en una nueva capital es de 100';
+    $con = mysqli_connect($host, $user, $pw, $db) or die('no se pudo autentificar con la Bd');
+    mysqli_select_db($con, $db) or die('no se puedo conectar a la BD');
+    $sql = "INSERT INTO usuarios(user,pass)
+        VALUES ('$user','$pass')";
+    if ($con->query($sql) === TRUE) {
+        echo 'guardado corretamente <br>';
+        echo '<a href=login.php> volver al inicio </a>';
+    } else {
+        echo 'error:' . $sql . '<br>' . $con->error;
     }
-}else{
+    $con->close();
+} else {
     echo 'debe de llenar los campos';
     echo '<a href=registrar.php> Volver a registrarse </a>';
 }
-?>
