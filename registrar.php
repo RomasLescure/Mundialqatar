c39250b400987e2a07e0229cc28e63a351389eea
rocica <rocio.cortez@utp.ac.pa>
2022-11-27 23:44:07 -0500

    conexión con las bd

--- registrar.php
+++ registrar.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
 
 <head>
     <meta charset="UTF-8">
@@ -11,13 +10,13 @@
 
 <body>
     <div class="card text-white bg-dark mb-3" style="margin: 20px 20px 20px 20px;">
-        <div class="card-header">Inicio de sesión</div>
+        <div class="card-header">IRegistro</div>
         <div class="card-body">
-            <form action="login.php" method="post">
+            <form action="registro.php" method="post">
                 <fieldset>
                     <div class="form-group">
                         <label for="user" class="form-label mt-4">Usuario</label>
-                        <input type="text" class="form-control" name="user" placeholder="Insertar usuario">
+                        <input type="text" class="form-control" name="user" placeholder="Insertar Usuario">
                     </div>
                     <div class="form-group">
                         <label for="pass" class="form-label mt-4">Contraseña</label>
@@ -26,16 +25,20 @@
                     <!-- Dependiendo del tipo de usuario se lleva a una pagina u a otra -->
                     <div class="form-group">
                         <label for="exampleSelect1" class="form-label mt-4">Tipo de usuario</label>
-                        <select class="form-select" name="Tipo_Usuario">
-                            <!-- Si es predeterminado se registra automaticamente -->
-                            <option>Predeterminado</option>
-                            <!-- Si es Administrativo se debe llevar a una pagina en la cual debe colocar
+                        <!-- Si es predeterminado se registra automaticamente -->
+                        <br><input type="radio" value="pred" id="pred" name="tipo">
+                        <label for="pred">Opcion Predeterminado</label>
+                        <!-- Si es Administrativo se debe llevar a una pagina en la cual debe colocar
                         una contraseña que solo los administrativos saben osea nosotros, elijan ustedes la 
                         contraseña -->
-                            <option>Administrativo</option>
-                        </select>
+                        <br><input type="radio" value="admin" id="admin" name="tipo">
+                        <label for="admin">Opcion Administrativo</label>
                     </div>
                     <br>
+                    <div class="form-group">
+                        <label for="adminPass" class="form-label mt-4">Contraseña administrativa</label>
+                        <input type="text" class="form-control" name="adminPass" placeholder="Insertar Contraseña">
+                    </div>
                     <input type="submit" value="Registrarse">
                 </fieldset>
             </form>
--- registro.php
+++ registro.php
@@ -1,27 +1,61 @@
 <?php
 if (
-    isset($_POST['user']) && !empty($_POST['user']) && isset($_POST['pass']) &&
-    !empty($_POST['pass'])
+    isset($_POST['user']) && !empty($_POST['user']) &&
+    isset($_POST['pass']) && !empty($_POST['pass']) &&
+    isset($_POST['tipo']) && !empty($_POST['tipo'])
 ) {
     $user = $_POST['user'];
     $pass = $_POST['pass'];
+    $type = $_POST['tipo'];
 
-    $db = 'qatar_2022';
     $host = 'localhost';
+    $db = 'qatar_2022';
     $pw = '';
-    $user = 'root';
+    $userroot = 'root';
 
-    $con = mysqli_connect($host, $user, $pw, $db) or die('no se pudo autentificar con la Bd');
+    $con = mysqli_connect($host, $userroot, $pw, $db) or die('no se pudo autentificar con la Bd');
     mysqli_select_db($con, $db) or die('no se puedo conectar a la BD');
-    $sql = "INSERT INTO usuarios(user,pass)
-        VALUES ('$user','$pass')";
-    if ($con->query($sql) === TRUE) {
-        echo 'guardado corretamente <br>';
-        echo '<a href=login.php> volver al inicio </a>';
+
+    if ($type == "pred") {
+        $sql = "INSERT INTO usuarios(user, pass, tipo)
+        VALUES ('$user','$pass', '$type')";
+
+        if ($con->query($sql) === TRUE) {
+            echo 'Se ha registrado exitosamente<br><br>';
+            echo '<a href=login.php> LOGIN </a>';
+            echo $type;
+        } else {
+            echo 'error:' . $sql . '<br>' . $con->error;
+        }
+        $con->close();
     } else {
-        echo 'error:' . $sql . '<br>' . $con->error;
+
+        if (
+            isset($_POST['adminPass']) && !empty($_POST['adminPass'])
+        ) {
+            $adminPass = $_POST['adminPass'];
+            if ($adminPass == "code") {
+                $sql = "INSERT INTO usuarios(user, pass, tipo)
+                VALUES ('$user','$pass','$type')";
+
+                if ($con->query($sql) === TRUE) {
+                    echo 'Se ha registrado exitosamente<br><br>';
+                    echo '<a href=login.php> LOGIN </a>';
+                    echo $type;
+                } else {
+                    echo 'error:' . $sql . '<br>' . $con->error;
+                }
+                $con->close();
+            } else {
+                echo 'Contraseña incorrecta<br>';
+                echo 'No tiene acceso a funciones administrativas y su información no fue almacenada<br>';
+                echo '<a href=registrar.php>Puede volver a Registrar</a>';
+            }
+        } else {
+            echo 'debe de llenar los campos';
+            echo '<a href=registrar.php> Volver a registrarse </a>';
+        }
     }
-    $con->close();
 } else {
     echo 'debe de llenar los campos';
     echo '<a href=registrar.php> Volver a registrarse </a>';
