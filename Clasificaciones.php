b53a9e60ea7ba32939cfbf90dcb9a0aeab21ffe8
juant14 <105187326+juant14@users.noreply.github.com>
2022-11-28 22:48:45 -0500

    Update Clasificaciones.php

--- Clasificaciones.php
+++ Clasificaciones.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,38 +8,40 @@
     <link rel="stylesheet" href="bootstrap.min.css">
     <title>CLASIFICACIONES</title>
 </head>
+
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-        <a class="navbar-brand" href="#">Qatar 2022</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarColor01">
-            <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="inicio.php">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="equipos.php">Equipos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="resultados.php">Resultados</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Posiciones.php">Tabla de posiciones por grupo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="favoritos.php">Favoritos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Clasificaciones</a>
-                <span class="visually-hidden">(current)</span>
-            </li>
-            </ul>
-            <a href="login.php"><button type="button" class="btn btn-secondary" style="margin: 0px 5px 0px 5px;">Login</button></a>
-            <a href="registrar.php"><button type="button" class="btn btn-secondary" style="margin: 0px 5px 0px 5px;">Register</button></a>
-        </div>
+            <a class="navbar-brand" href="#">Qatar 2022</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Inicio
+                            <span class="visually-hidden">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="equipos.php">Equipos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Resultados</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tabla de posiciones por grupo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Favoritos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clasificaciones</a>
+                    </li>
+                </ul>
+                <a href="login.php"><button type="button" class="btn btn-secondary" style="margin: 0px 5px 0px 5px;">Login</button></a>
+                <a href="registrar.php"><button type="button" class="btn btn-secondary" style="margin: 0px 5px 0px 5px;">Register</button></a>
+            </div>
         </div>
     </nav>
   <br>
