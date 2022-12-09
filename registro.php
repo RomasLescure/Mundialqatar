a0211ce1c2996c0e1e62baa555a3fdc210a704b5
juant14 <105187326+juant14@users.noreply.github.com>
2022-12-08 22:13:16 -0500

    validacion, contraseña de admin

--- registro.php
+++ registro.php
@@ -2,12 +2,13 @@
 if (
     isset($_POST['user']) && !empty($_POST['user']) &&
     isset($_POST['pass']) && !empty($_POST['pass']) &&
-    isset($_POST['tipo']) && !empty($_POST['tipo'])
+    isset($_POST['tipo']) && !empty($_POST['tipo']) &&
+    isset($_POST['adminPass']) && !empty($_POST['adminPass'])
 ) {
     $user = $_POST['user'];
     $pass = $_POST['pass'];
     $type = $_POST['tipo'];
-
+    $adminPass = $_POST['adminPass'];
     $host = 'localhost';
     $db = 'datos_mundial';
     $pw = '';
@@ -30,10 +31,7 @@ if (
         $con->close();
     } else {
 
-        if (
-            isset($_POST['adminPass']) && !empty($_POST['adminPass'])
-        ) {
-            $adminPass = $_POST['adminPass'];
+     
             if ($adminPass == "code") {
                 $sql = "INSERT INTO usuarios(user, pass, tipo)
                 VALUES ('$user','$pass','$type')";
@@ -45,16 +43,13 @@ if (
                 } else {
                     echo 'error:' . $sql . '<br>' . $con->error;
                 }
-                $con->close();
+                
             } else {
                 echo 'Contraseña incorrecta<br>';
                 echo 'No tiene acceso a funciones administrativas y su información no fue almacenada<br>';
                 echo '<a href=registrar.php>Puede volver a Registrar</a>';
             }
-        } else {
-            echo 'debe de llenar los campos';
-            echo '<a href=registrar.php> Volver a registrarse </a>';
-        }
+            $con->close();
     }
 } else {
     echo 'debe de llenar los campos';
